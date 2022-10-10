c9b5d7c4c737a8b8ffd3392818d0ccefb4e5d1de
Victor Martinez <victormartinobre@hotmail.com>
2022-10-10 20:11:35 -0300

    feat: :construction: add-images-public-assets

--- 3_advancingInReact/advancing/src/App.js
+++ 3_advancingInReact/advancing/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import City from './assets/city.jpg';
 
 function App() {
   return (
     <div className="App">
       <h1>Seção 3</h1>
+      {/** Imagem em public */}
+      <div>
+        <img src="/img1.jpg" alt="Bela paisagem" />
+      </div>
+      {/** Imagem com assets */}
+        <img src={City} alt="Bela Cidade" />
     </div>
   );
 }

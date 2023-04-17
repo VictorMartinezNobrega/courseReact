be91110622a92ef52cf7e741c750f01d5f07cc5f
Victor Martinez <victormartinobre@hotmail.com>
2023-04-17 19:48:23 -0300

    complete-css-in-react-and-starting-challenge

---
+++ 4_CSS_react/cssInReact/src/components/MyTittle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MyTittle.module.css'
+
+const MyTittle = () => {
+  return (
+    <div>
+        <h1 className={styles.tittle_master}>Meu titulo master</h1>
+    </div>
+  )
+}
+
+export default MyTittle;
